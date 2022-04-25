a60ea80297cada56cfe719b041dc698e81a0ed51
Antoha12011 <toni.pavlov.85@bk.ru>
2022-04-25 19:54:34 +0300

    All right

--- SwiftUI VR/ContentView.swift
+++ SwiftUI VR/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(animals) { animal in
+                NavigationLink(destination: DetailView(animal: animal)) {
+                    HStack {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Text("\(animal.title)")
+                                .font(.system(size: 18, weight: .bold))
+                            
+                            Text("\(animal.subtitle)")
+                        }
+                        Spacer()
+                        
+                        Image(animal.image)
+                            .resizable()
+                            .frame(width: 100, height: 100)
+                            .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                    }
+                }
+            }
+            .navigationBarTitle("Комплексы")
+        }
     }
 }
 
@@ -19,3 +38,16 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct ListItem: Identifiable {
+    var id = UUID()
+    var title: String
+    var subtitle: String
+    var image: String
+}
+
+var animals = [
+    ListItem(title: "Жилой комплекс", subtitle: "Карякина", image: "Дом"),
+    ListItem(title: "Жилой комплекс", subtitle: "Краснодар", image: "Дома"),
+    ListItem(title: "Жилой комплекс", subtitle: "Кубанский", image: "Домаа")]
+
---
+++ SwiftUI VR/PopupView.swift
@@ -0,0 +1,21 @@
+//
+//  PopupView.swift
+//  SwiftUI VR
+//
+//  Created by Anton Pavlov on 25.04.2022.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PopupView()
+    }
+}
+
